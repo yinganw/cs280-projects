56e4b27abaf02c0a6936c26a69d315a2495dc1ef
Yingan Wang <yinganwang@Yingans-MacBook-Pro.local>
2025-09-02 21:58:22 -0700

    fix: deployment setting

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next'
 
-const isProd = process.env.NODE_ENV === 'production'
-const repo = 'yinganw.github.io' // 🔁 Replace with your GitHub username
+// const isProd = process.env.NODE_ENV === 'production'
+// const repo = 'yinganw.github.io' // 🔁 Replace with your GitHub username
 
 const nextConfig: NextConfig = {
-  assetPrefix: isProd ? `/${repo}/` : '',
-  basePath: isProd ? `/${repo}` : '',
+  // assetPrefix: isProd ? `/${repo}/` : '',
+  // basePath: isProd ? `/${repo}` : '',
   trailingSlash: true,
 }
 
